e077bb5843e69e85b8fe597e5bd90bc354071a05
Azanen <60452086+Azanen@users.noreply.github.com>
2022-07-15 19:23:26 -0400

    Drag, drop AND snap

--- GMTK2022/Assets/Philippe_Folder/Drag_Object.cs
+++ GMTK2022/Assets/Philippe_Folder/Drag_Object.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Drag_Object : MonoBehaviour
 {
     private Vector3 mOffSet; 
-    private float mZCoord; 
+    private float mZCoord;
+    public string destinationTag = "DropArea";
     
     private void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         mOffSet = gameObject.transform.position - GetMouseWorldPos();
+        transform.GetComponent<Collider>().enabled = false;
+        Cursor.visible = false;
     }
 
     private Vector3 GetMouseWorldPos()
@@ -26,4 +29,34 @@ public class Drag_Object : MonoBehaviour
     {
         transform.position = GetMouseWorldPos() + mOffSet;
     }
+
+    private void OnMouseUp()
+    {
+        var rayOrigin = Camera.main.transform.position;
+        var rayDirection = GetMouseWorldPos() - Camera.main.transform.position;
+        RaycastHit hitInfo;
+        if(Physics.Raycast(rayOrigin, rayDirection, out hitInfo))
+        {
+            if(hitInfo.transform.tag == destinationTag)
+            {
+                transform.position = hitInfo.transform.position;
+            }
+        }
+
+        transform.GetComponent<Collider>().enabled = true;
+        Cursor.visible = true;
+    }
+
+    private void Update()
+    {
+        
+    if (Input.GetMouseButtonDown(1))
+            {
+              transform.rotation = Quaternion.Euler(new Vector3(
+              transform.rotation.eulerAngles.x,
+              transform.rotation.eulerAngles.y + 90f,
+              transform.rotation.eulerAngles.z));
+            }
+    }
+
 }
