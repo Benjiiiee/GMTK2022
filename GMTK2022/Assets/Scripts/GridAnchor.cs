b332ad8fddb033cd0d22d1e84a7bc7957114ff81
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2022-07-15 21:04:08 -0400

    GridManager

---
+++ GMTK2022/Assets/Scripts/GridAnchor.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridAnchor : MonoBehaviour
+{
+    public Vector3Int gridPosition;
+}
--- GMTK2022/Assets/Scripts/GridManager.cs
+++ GMTK2022/Assets/Scripts/GridManager.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     public float distance = 1f;
-    public Vector3 gridSize;
+    public Vector3Int gridSize;
+    public GridAnchor gridAnchor;
+    public GridAnchor[,,] gridArray;
 
-    public void Start()
+    public void InitiateGrid()
     {
-        
+        gridArray = new GridAnchor[gridSize.x, gridSize.y, gridSize.z];
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int z = 0; z < gridSize.z; z++)
+            {
+                for (int x = 0; x < gridSize.x; x++)
+                {
+                    if (gridAnchor != null)
+                    {
+                        GameObject temp = Instantiate(gridAnchor.gameObject, new Vector3(x, y, z), Quaternion.identity);
+                        gridArray[x, y, z] = temp.GetComponent<GridAnchor>();
+                        temp.GetComponent<GridAnchor>().gridPosition = new Vector3Int(x, y, z);
+                        
+                    }
+                }
+            }
+        }
     }
 
 
