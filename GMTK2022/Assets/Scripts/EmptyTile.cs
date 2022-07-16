d41b0bba41236bbb1c8e62d453eabae254e8f8ae
Louis Nantel <nantel.louis@gmail.com>
2022-07-16 13:57:30 -0400

    Fixed: die would throw itself off ledges with 0 movement left

--- GMTK2022/Assets/Scripts/EmptyTile.cs
+++ GMTK2022/Assets/Scripts/EmptyTile.cs
@@ -11,16 +11,31 @@ public class EmptyTile : Tile
     }
 
     public override bool EnterTile(DieController die, out Vector3Int nextDirection, out int nextSteps, out Vector3Int nextGridPosition) {
-        if (die.Steps <= 0) {
-            nextDirection = new Vector3Int(0, -1, 0);
-            nextSteps = die.Steps;
+        // If entering an EmptyTile from the side:
+        if (die.Direction.x != 0 || die.Direction.z != 0) {
+            // Check if the die can pay the movement cost:
+            if(die.Steps >= MovementCost) {
+                // If it can, it pays the cost and moves into this tile
+                nextSteps = die.Steps - MovementCost;
+                nextGridPosition = GridPosition;
+                // If it has no steps left, change its direction to down
+                nextDirection = nextSteps == 0 ? new Vector3Int(0, -1, 0) : new Vector3Int(die.Direction.x, -1, die.Direction.z);
+                return true;
+            }
+            else {
+                // If it can't pay the cost, check if it can fall instead
+                die.Direction = new Vector3Int(0, -1, 0);
+                return LevelManager.instance.GetTileInDirection(die.GridPosition, Vector3Int.down).EnterTile(die, out nextDirection, out nextSteps, out nextGridPosition);
+            }
         }
+        // If entering an EmptyTile from the top:
         else {
-            nextDirection = new Vector3Int(die.Direction.x, -1, die.Direction.z);
-            nextSteps = die.Steps - MovementCost;
+            // Fall
+            nextDirection = die.Direction;
+            nextSteps = die.Steps;
+            nextGridPosition = GridPosition;
+            return true;
         }
-        nextGridPosition = GridPosition;
-        return true;
     }
 
 }
