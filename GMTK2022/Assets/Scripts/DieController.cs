fcde1e15939802f0db4409417be8a3f1fe51e43b
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2022-07-16 22:20:08 -0400

    Dice Anim integration

--- GMTK2022/Assets/Scripts/DieController.cs
+++ GMTK2022/Assets/Scripts/DieController.cs
@@ -24,33 +24,40 @@ public class DieController : MonoBehaviour
     public GameObject D6Mesh;
     public GameObject D8Mesh;
     private TextMeshProUGUI TXT_FaceValue;
+    private Animator animator;
 
     public DirectionArrow[] DirectionArrows;
 
     private IEnumerator MovementCoroutine;
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         StoredDiceController.RetrieveDie += RetrieveDie;
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         StoredDiceController.RetrieveDie -= RetrieveDie;
     }
 
-    public void Spawn(Vector3Int StartPosition, DieTypes StartingType, int StartingFaceValue) {
+    public void Spawn(Vector3Int StartPosition, DieTypes StartingType, int StartingFaceValue)
+    {
         GridPosition = StartPosition;
         transform.position = StartPosition;
         DieType = StartingType;
         FaceValue = StartingFaceValue;
 
-        if(StoreDie != null) {
+        if (StoreDie != null)
+        {
             if (StartingType != DieTypes.D4) StoreDie(DieTypes.D4, 4);
             if (StartingType != DieTypes.D6) StoreDie(DieTypes.D6, 6);
             if (StartingType != DieTypes.D8) StoreDie(DieTypes.D8, 8);
         }
     }
-    private void ChangeDieType(DieTypes newType, int faceValue) {
-        switch (DieType) {
+    private void ChangeDieType(DieTypes newType, int faceValue)
+    {
+        switch (DieType)
+        {
             case DieTypes.D4:
                 D4Mesh.SetActive(false);
                 break;
@@ -64,17 +71,21 @@ public class DieController : MonoBehaviour
                 break;
         }
 
-        switch (newType) {
+        switch (newType)
+        {
             case DieTypes.D4:
                 D4Mesh.SetActive(true);
+                animator = D4Mesh.GetComponentInChildren<Animator>();
                 TXT_FaceValue = D4Mesh.GetComponentInChildren<TextMeshProUGUI>();
                 break;
             case DieTypes.D6:
                 D6Mesh.SetActive(true);
+                animator = D6Mesh.GetComponentInChildren<Animator>();
                 TXT_FaceValue = D6Mesh.GetComponentInChildren<TextMeshProUGUI>();
                 break;
             case DieTypes.D8:
                 D8Mesh.SetActive(true);
+                animator = D8Mesh.GetComponentInChildren<Animator>();
                 TXT_FaceValue = D8Mesh.GetComponentInChildren<TextMeshProUGUI>();
                 break;
             default:
@@ -85,39 +96,52 @@ public class DieController : MonoBehaviour
         CurrentDieType = newType;
     }
 
-    private void RetrieveDie(DieTypes type, int faceValue) {
+    private void RetrieveDie(DieTypes type, int faceValue)
+    {
         if (StoreDie != null) StoreDie(DieType, FaceValue);
         ChangeDieType(type, faceValue);
     }
 
-    private void SetDirection(Vector3Int newDir) {
+    private void SetDirection(Vector3Int newDir)
+    {
         CurrentDirection = newDir;
         if (newDir.x != 0 || newDir.z != 0) LastHorizontalDirection = new Vector3Int(newDir.x, 0, newDir.z);
     }
 
-    public void Shoot(Vector3Int direction) {
+    public void Shoot(Vector3Int direction)
+    {
         Direction = direction;
         Steps = FaceValue;
-        if (MovementCoroutine == null) {
+        if (MovementCoroutine == null)
+        {
             MovementCoroutine = Movement();
             StartCoroutine(MovementCoroutine);
         }
+        if (animator != null)
+        {
+            animator.SetTrigger("isrun");
+        }
     }
 
-    private void HideDirectionArrows() {
-        foreach(DirectionArrow arrow in DirectionArrows) {
+    private void HideDirectionArrows()
+    {
+        foreach (DirectionArrow arrow in DirectionArrows)
+        {
             arrow.Hide();
         }
     }
 
-    private void ShowDirectionArrows() {
-        foreach (DirectionArrow arrow in DirectionArrows) {
+    private void ShowDirectionArrows()
+    {
+        foreach (DirectionArrow arrow in DirectionArrows)
+        {
             arrow.Show();
         }
     }
 
     // Returns true if the die moved successfully; returns false if it stopped
-    public bool NextStep() {
+    public bool NextStep()
+    {
         // Look up the next tile in the current direction
         Tile nextTile = LevelManager.instance.GetTileInDirection(GridPosition, Direction);
 
@@ -133,16 +157,19 @@ public class DieController : MonoBehaviour
         return HasMoved;
     }
 
-    private IEnumerator Movement() {
+    private IEnumerator Movement()
+    {
         HideDirectionArrows();
         HideFaceValue();
         Vector3Int LastGridPosition = GridPosition;
 
         // Begin movement
-        while (NextStep()) {
+        while (NextStep())
+        {
             float moveTimer = 0f;
             float moveDuration = 0.5f;
-            while(moveTimer < moveDuration) {
+            while (moveTimer < moveDuration)
+            {
                 transform.position = Vector3.Lerp(LastGridPosition, GridPosition, moveTimer / moveDuration);
                 yield return null;
                 moveTimer += Time.deltaTime;
@@ -153,19 +180,27 @@ public class DieController : MonoBehaviour
         }
 
         // Movement finished
-        if(MoveCompleted != null) MoveCompleted();
+        if (MoveCompleted != null) MoveCompleted();
         ShowDirectionArrows();
         RollDie();
         ShowFaceValue();
+        if (animator != null)
+        {
+            animator.SetTrigger("isidle");
+        }
         MovementCoroutine = null;
+
     }
 
-    private void RollDie() {
+    private void RollDie()
+    {
         FaceValue = UnityEngine.Random.Range(1, GetMaxFaceValue());
     }
 
-    private int GetMaxFaceValue() {
-        switch (DieType) {
+    private int GetMaxFaceValue()
+    {
+        switch (DieType)
+        {
             case DieTypes.D4:
                 return 4;
             case DieTypes.D6:
@@ -177,20 +212,24 @@ public class DieController : MonoBehaviour
         }
     }
 
-    private void SetFaceValue(int value) {
+    private void SetFaceValue(int value)
+    {
         CurrentFaceValue = value;
         if (TXT_FaceValue != null) TXT_FaceValue.text = value.ToString();
     }
 
-    private void HideFaceValue() {
+    private void HideFaceValue()
+    {
         if (TXT_FaceValue != null) TXT_FaceValue.enabled = false;
     }
 
-    private void ShowFaceValue() {
+    private void ShowFaceValue()
+    {
         if (TXT_FaceValue != null) TXT_FaceValue.enabled = true;
     }
 
-    public void StopMovement() {
+    public void StopMovement()
+    {
         if (MovementCoroutine != null) StopCoroutine(MovementCoroutine);
     }
 }
